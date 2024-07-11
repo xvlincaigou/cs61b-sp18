89c3cdf469adc5303195cf7380948cf2c1d56c81
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-07-11 15:07:10 +0800

    lab11完成Astar

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.PriorityQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -20,7 +24,7 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +35,28 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        PriorityQueue<Integer> q = new PriorityQueue<>((v1, v2) -> {
+            return Integer.compare(distTo[v1] + h(v1), distTo[v2] + h(v2));
+        });
+        q.add(s);
+        while (!q.isEmpty()) {
+            int v = q.poll();
+            marked[v] = true;
+            announce();
+            if (v == t) {
+                targetFound = true;
+                return;
+            }
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    if (distTo[w] > distTo[v] + 1) {
+                        distTo[w] = distTo[v] + 1;
+                        edgeTo[w] = v;
+                        q.add(w);
+                    }
+                }
+            }
+        }
     }
 
     @Override
