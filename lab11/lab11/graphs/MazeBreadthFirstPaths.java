04cfc28b057d842d31894d601910f2ff58d026ee
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-07-11 14:40:25 +0800

    lab11完成BFS

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +11,48 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int s;
+    private int t;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
+        Queue<Integer> q = new Queue<>();
+        int s = maze.xyTo1D(1, 1);
+        q.enqueue(s);
+        marked[s] = true;
+        announce();
+
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            marked[v] = true;
+            announce();
+            if (v == t)
+                break;
+            for (int neighbor : maze.adj(v)) {
+                if (marked[neighbor])
+                    continue;
+                q.enqueue(neighbor);
+                distTo[neighbor] = distTo[v] + 1;
+                edgeTo[neighbor] = v;
+            }
+        }
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -2,6 +2,8 @@ package lab14;
 
 public class Main {
 	public static void main(String[] args) {
-		/** Your code here. */
+		Generator generator = new SineWaveGenerator(440);
+		GeneratorPlayer gp = new GeneratorPlayer(generator);
+		gp.play(1000000);
 	}
 } 
